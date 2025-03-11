bf5acf571eb743f1172d154ab88c1d549c6d5cf1
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-11 15:45:57 -0400

    Add subtract, multiply, and divide function

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,15 @@ console.log("Hello world!")
 function add(num1, num2){
     return num1 + num2;
 }
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
