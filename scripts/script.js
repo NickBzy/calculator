f217e17999190df45745e6ec0da847851f714bcc
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-12 13:37:23 -0400

    Add UI for calculator

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,19 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1/num2;
 }
+
+let number1;
+let number2;
+let operator;
+
+function operate(operator, num1, num2){
+    if (operator === '+'){
+        return add(num1, num2)
+    } else if (operator === '-'){
+        return subtract(num1, num2)
+    } else if (operator === '*'){
+        return multiply(num1, num2)
+    } else if (operator === '/'){
+        return divide(num1, num2)
+    }
+}
